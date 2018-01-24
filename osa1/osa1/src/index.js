9910f95e23d2c0017691cbfee744481e5c8f0b61
Marko Koskinen <markokoskinen2037@gmail.com>
2018-01-24 19:29:00 +0200

    1.4

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -3,24 +3,26 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = {
-    nimi: 'Reactin perusteet',
-    tehtavia: 10
-  }
-  const osa2 = {
-    nimi: 'Tiedonvälitys propseilla',
-    tehtavia: 7
-  }
-  const osa3 = {
-    nimi: 'Komponenttien tila',
-    tehtavia: 14
-  }
+  const osat = [
+    {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    },
+    {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    },
+    {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
+  ]
 
   return (
     <div>
       <Otsikko kurssi={kurssi}/>
-      <Sisalto osa1={osa1.nimi} osa2={osa2.nimi} osa3={osa3.nimi} tehtavia1={osa1.tehtavia} tehtavia2={osa2.tehtavia} tehtavia3={osa3.tehtavia} />
-      <Yhteensa tehtavia1={osa1.tehtavia} tehtavia2={osa2.tehtavia} tehtavia3={osa3.tehtavia}/>
+      <Sisalto osat={osat} />
+      <Yhteensa osat={osat}/>
     </div>
   )
 }
@@ -36,17 +38,21 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
   return (
     <div>
-      <Osa nimi={props.osa1} tehtavamaara={props.tehtavia1}/>
-      <Osa nimi={props.osa2} tehtavamaara={props.tehtavia2}/>
-      <Osa nimi={props.osa3} tehtavamaara={props.tehtavia3}/>
+      <Osa nimi={props.osat[0].nimi} tehtavamaara={props.osat[0].tehtavia}/>
+      <Osa nimi={props.osat[1].nimi} tehtavamaara={props.osat[1].tehtavia}/>
+      <Osa nimi={props.osat[2].nimi} tehtavamaara={props.osat[2].tehtavia}/>
     </div>
   )
 }
 
 const Yhteensa = (props) => {
+  let sum = 0;
+  props.osat.forEach(element => {
+     sum = sum + element.tehtavia;
+  });
   return (
     <div>
-      <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+      <p>yhteensä {sum} tehtävää</p>
     </div>
   )
 }
