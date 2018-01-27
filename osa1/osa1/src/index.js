b8f4b6a70e0b2485fe478916b360fe064608dca1
Marko Koskinen <markokoskinen2037@gmail.com>
2018-01-27 17:31:33 +0200

    1.14

--- osa1/osa1/src/index.js
+++ osa1/osa1/src/index.js
@@ -6,23 +6,37 @@ class App extends React.Component {
     super(props)
     this.state = {
       selected: 0,
-      pisteet: [0,1,2,3,4,5]
+      best: 0,
+      pisteet: [0, 0, 0, 0, 0, 0]
     }
   }
 
   randomAnectode = () => {
     let random = Math.floor(Math.random() * 5) + 1;
-    this.setState({selected: random})
+    this.setState({ selected: random })
   }
 
   giveVoteToCurrent = () => {
     const kopio = [...this.state.pisteet]
-    kopio[this.state.selected] = kopio[this.state.selected]+1;
-    this.setState({ pisteet: kopio})
-    console.log(kopio)
+    kopio[this.state.selected] = kopio[this.state.selected] + 1;
+    this.setState({ pisteet: kopio })
+    this.updateBest()
   }
 
+  updateBest = () => {
 
+    let i = 0;
+    let largest = 0;
+    while (i < this.state.pisteet.length) {
+      if (this.state.pisteet[i] >= largest) {
+        largest = this.state.pisteet[i];
+        this.setState({ best: i })
+      }
+      i++;
+    }
+
+
+  }
 
 
   render() {
@@ -32,6 +46,10 @@ class App extends React.Component {
         <br></br>
         <button onClick={this.randomAnectode}>next anecdote</button>
         <button onClick={this.giveVoteToCurrent}>give +1</button>
+        <h1>anecdoote with most votes:</h1>
+        {anecdotes[this.state.best]}
+        <br></br>
+        <p>has {this.state.pisteet[this.state.best]} votes</p>
       </div>
     )
   }
