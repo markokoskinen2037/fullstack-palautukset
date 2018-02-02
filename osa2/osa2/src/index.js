dfb2eb2b2ecf40278382d12eee213e7deba5367e
Marko Koskinen <markokoskinen2037@gmail.com>
2018-02-02 15:45:28 +0200

    2.4

--- osa2/osa2/src/index.js
+++ osa2/osa2/src/index.js
@@ -4,33 +4,64 @@ import './index.css';
 
 
 const App = () => {
-    const kurssi = {
-      nimi: 'Half Stack -sovelluskehitys',
-      osat: [
+    const kurssit = [
         {
-          nimi: 'Reactin perusteet',
-          tehtavia: 10,
-          id: 1
+          nimi: 'Half Stack -sovelluskehitys',
+          id: 1,
+          osat: [
+            {
+              nimi: 'Reactin perusteet',
+              tehtavia: 10,
+              id: 1
+            },
+            {
+              nimi: 'Tiedonvälitys propseilla',
+              tehtavia: 7,
+              id: 2
+            },
+            {
+              nimi: 'Komponenttien tila',
+              tehtavia: 14,
+              id: 3
+            }
+          ]
         },
         {
-          nimi: 'Tiedonvälitys propseilla',
-          tehtavia: 7,
-          id: 2
-        },
-        {
-          nimi: 'Komponenttien tila',
-          tehtavia: 14,
-          id: 3
+          nimi: 'Node.js',
+          id: 2,
+          osat: [
+            {
+              nimi: 'Routing',
+              tehtavia: 3,
+              id: 1
+            },
+            {
+              nimi: 'Middlewaret',
+              tehtavia: 7,
+              id: 2
+            }
+          ]
         }
       ]
-    }
   
     return (
       <div>
-        <Kurssi kurssi={kurssi} />
+          <h1>Opetusohjelma</h1>
+        <Kurssit kurssit={kurssit}/>
       </div>
     )
   }
+
+const Kurssit = ({kurssit}) => {
+    return (
+        kurssit.map(kurssi =>
+            <div>
+                <Kurssi kurssi={kurssi}/>
+            </div>
+        )
+        
+    )
+}
 
 
 const Kurssi = ({kurssi}) => {
