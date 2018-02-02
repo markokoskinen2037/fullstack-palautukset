fa7d8e6d9519ffe95316b2e8ad11bce9838bb87b
Marko Koskinen <markokoskinen2037@gmail.com>
2018-02-02 14:10:42 +0200

    2.1

---
+++ osa2/osa2/src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const App = () => {
+    const kurssi = {
+      nimi: 'Half Stack -sovelluskehitys',
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        }
+      ]
+    }
+  
+    return (
+      <div>
+        <Kurssi kurssi={kurssi} />
+      </div>
+    )
+  }
+
+
+const Kurssi = ({kurssi}) => {
+    return (
+        <div>
+            <h1>{kurssi.nimi}</h1>
+            {kurssi.osat.map(osa => <p key={osa.id}>{osa.nimi} {osa.tehtavia}</p>)}
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
