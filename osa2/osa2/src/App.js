9145381bf25271a37ef11717e70724cc8e1d930c
Marko Koskinen <markokoskinen2037@gmail.com>
2018-02-04 16:26:53 +0200

    2.8

--- osa2/osa2/src/App.js
+++ osa2/osa2/src/App.js
@@ -5,9 +5,10 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas', id: 'Arto Hellas'}
+        { name: 'Arto Hellas', id: 'Arto Hellas', number: 112 }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
@@ -17,17 +18,18 @@ class App extends React.Component {
 
     const personObject = {
       name: this.state.newName,
-      id: this.state.newName
+      id: this.state.newName,
+      number: this.state.newNumber
     }
 
     let dublicate = false
     this.state.persons.forEach(person => {
-      if(person.name === personObject.name){
+      if (person.name === personObject.name) {
         dublicate = true;
       }
     });
-    
-    if(!dublicate){ 
+
+    if (!dublicate) {
       const persons = this.state.persons.concat(personObject)
       this.setState({
         persons,
@@ -38,30 +40,40 @@ class App extends React.Component {
         newName: ""
       })
     }
-    
+
 
 
   }
 
   handleNameChange = (event) => {
-    this.setState({newName: event.target.value})
+    this.setState({ newName: event.target.value })
+  }
+
+  handleNumberChange = (event) => {
+    this.setState({ newNumber: event.target.value })
   }
 
   render() {
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.addPerson} onChange={this.handleNameChange}>
-          <div>
+
+        <form onSubmit={this.addPerson} >
+          <div onChange={this.handleNameChange}>
             nimi: <input value={this.state.newName} />
+          </div>
+          <div onChange={this.handleNumberChange}>
+            numero: <input />
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
+
+
         <h2>Numerot</h2>
         <ul>
-          {this.state.persons.map(person => <li key={person.name}>{person.name}</li>)}
+          {this.state.persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
         </ul>
       </div>
     )
